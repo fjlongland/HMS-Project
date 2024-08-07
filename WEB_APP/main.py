7f2c42da801d1dfdab94c9a38629c18e3264bba1
fjlongland <fjlongland@gmail.com>
2024-08-07 18:47:27 +0200

    Added TODO list for tomorrow

--- WEB_APP/main.py
+++ WEB_APP/main.py
@@ -1,1 +1,9 @@
 print("Hello world")
+
+#TODO: look into what DB to use and 
+#make shure the cloud hosting works.
+
+#TODO: build user table and finish
+# user registration and login.(Auth can be handled later)
+
+#TODO: figer out what to do next.
