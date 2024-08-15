5f5ca2a33238f21e34103f212639267108f740f3
fjlongland <fjlongland@gmail.com>
2024-08-15 17:40:58 +0200

    Finished adding all CRUD opperations for users added response model and added comments

--- CORE/schemas.py
+++ CORE/schemas.py
@@ -1,14 +1,23 @@
 from pydantic import BaseModel
 
-
 class User(BaseModel):
     username: str
     password: str
 
     class Config:
-        orm_model = True
+        orm_mode = True  
 
 class UserCreate(BaseModel):
     username: str
     password: str
     type: str
+
+    class Config:
+        orm_mode = True  
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True  
